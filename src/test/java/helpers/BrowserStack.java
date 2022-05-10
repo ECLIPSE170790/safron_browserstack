a798a74db154061d4c5e80fe97510703c0acc3bb
ECLIPSE170790 <eclipse170790@mail.ru>
2022-05-10 17:15:26 +0600

    Initial commit

---
+++ src/test/java/helpers/BrowserStack.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import static io.restassured.RestAssured.given;
+
+public class BrowserStack {
+    public static String videoUrl(String sessionId) {
+        return given()
+                .auth().basic("ekaterinasafrono_PELdAm", "qnyVs5Vi2xWHsXk2g3v6")
+                .when()
+                .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract()
+                .response()
+                .path("automation_session.video_url");
+
+    }
+}
---
+++ src/test/java/tests/BrowserStackAndroidSelenideTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class BrowserStackAndroidSelenideTests extends TestBase {
+
+    @Test
+    void searchTest() {
+        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
+                .shouldHave(sizeGreaterThan(0));
+    }
+}
